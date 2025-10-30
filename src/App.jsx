a3618e6bc2aa757fcafb7799d90bd1277cc8d0f8
raksha-jat <rakshaj.diploma24@ssism.org>
2025-10-30 13:12:50 +0530

    UserComponent file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import MyApp from './Headers'
+// import MyApp from './Headers'
+// function App(){
+//      return (
+//       <div>
+//         <MyApp />
+//       <h1>hello</h1>
+//       <h1>hello from react</h1></div>
+//      )
+     
+     
+// }
+// export default App;
+import Login,{SignUp,Profile,UserKey} from './UserComponent'
 function App(){
-     return (
-      <div>
-        <MyApp />
-      <h1>hello</h1>
-      <h1>hello from react</h1></div>
-     )
-     
-     
+  console.log(sum());
+  return(
+    <div>
+    <h1>FIRST Component :  {sum()}</h1>
+    <Fruit></Fruit>
+    <h1>{UserKey}</h1>
+    <Colors />
+    <Login />
+    <SignUp />
+    <Profile />
+        </div>
+  )
+}
+
+function Fruit(){
+  return(
+    <h1>Apple</h1>
+  )
+}
+function Colors(){
+  return(
+   <ul>
+    <li>Blue</li>
+    <li>Red</li>
+   </ul>
+  )
+}
+function sum(){
+  return "testing js in react"
 }
 export default App;
